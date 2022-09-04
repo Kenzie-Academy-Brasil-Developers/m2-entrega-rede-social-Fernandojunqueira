a20cb83d7529fd656e828e1238c3aaa9f752fb8f
Fernandojunqueira <fernandojunqueira12@gmail.com>
2022-09-04 16:15:35 -0300

    feat: add funcionality for buttons register and login

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,15 @@
+const btnLogin = document.getElementById('btnLogin')
+const btnRegister = document.getElementById('btnRegister')
 
+btnLogin.addEventListener('click', () => {
+    const formLogin = document.querySelector('.login')
+    const formRegister = document.querySelector('.register')
+    formLogin.classList.toggle('hidden')
+    formRegister.classList.toggle('hidden')
+})
+btnRegister.addEventListener('click', () => {
+    const formLogin = document.querySelector('.login')
+    const formRegister = document.querySelector('.register')
+    formLogin.classList.toggle('hidden')
+    formRegister.classList.toggle('hidden')
+})
