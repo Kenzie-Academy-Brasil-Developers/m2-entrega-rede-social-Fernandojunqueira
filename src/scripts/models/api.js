6c5463eff8a62858857aff70de033db4e29f3148
Fernandojunqueira <fernandojunqueira12@gmail.com>
2022-09-11 15:19:06 -0300

    add: html and style from mobile Dashboard

--- src/scripts/models/api.js
+++ src/scripts/models/api.js
@@ -3,8 +3,8 @@ export class ApiRequests {
     static baseUrl = 'https://m2-rede-social.herokuapp.com/api/'
     static token = localStorage.getItem("@kenzieRedeSocial:token") || "";
     static headers = {
-        "Content-Type": "application/json"
-        // Authorization: `token ${this.token}`
+        "Content-Type": "application/json",
+        "Authorization": `Token ${this.token}`
     }
 
     static async registerLogin(body) {
